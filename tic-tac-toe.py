f88e040de5b0e6c8eeaf99ecdb4fe91c144d4d2f
StevenCopeland <steven@thomasleecopeland.com>
2017-05-22 16:45:24 -0400

    Adding another twenty lines of code

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -135,4 +135,27 @@ while True:
     gameIsPlaying = True
 
     while gameIsPlaying:
-        
+        if turn == 'player':
+            # Player's turn
+            drawBoard(theBoard)
+            move = getPlayerMove(theBoard)
+            makeMove(theBoard, playerLetter, move)
+
+            if isWinner(theBoard, playerLetter):
+                drawBoard(theBoard)
+                print("Hooray! You have won the game!")
+                gameIsPlaying = False
+            else:
+                if isBoardFull(theBoard):
+                    drawBoard(theBoard)
+                    print("The game is a tie!")
+                    break
+                else:
+                    turn = "computer"
+            
+        else:
+            # Computer's turn
+            move = getComputerMove(theBoard, computerLetter)
+            makeMove(theBoard, computerLetter, move)
+
+            
