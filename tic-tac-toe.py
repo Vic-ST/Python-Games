d9d292397275099922795b4b4752eebea237f760
StevenCopeland <steven@thomasleecopeland.com>
2017-05-22 13:18:51 -0400

    Adding some code

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,11 @@
+# Tic-Tac-Toe
+
 import random
+
+def drawBoard(board):
+    # this function prints the board
+
+    # "board" is a list of strings
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    
