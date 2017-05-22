308ff40349dc4b97f5a05dc76b1300b68b497b23
StevenCopeland <steven@thomasleecopeland.com>
2017-05-22 13:27:10 -0400

    finishing a function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,4 +8,23 @@ def drawBoard(board):
     # "board" is a list of strings
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
-    
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[3] + '|' + board[2] + '|' + board[1])
+
+def inputPlayerLetter():
+    #player choose letter(X or O)
+
+    letter = ''
+    while not (letter == "X" or letter == "O"):
+        print("Do you want to be X or O")
+        letter = input().upper()
+
+        #first element players letter; second computers letter
+        if letter == "X":
+            return ["X", "O"]
+        else:
+            return ["O", "X"]
+
+def whoGoesFirst():
+    # rand choose who goes first
