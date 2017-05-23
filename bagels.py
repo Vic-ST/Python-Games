70ca7158a44f178b07e45dd2851007a8733001a4
StevenCopeland <steven@thomasleecopeland.com>
2017-05-23 13:23:52 -0400

    finishing the last function

--- bagels.py
+++ bagels.py
@@ -31,4 +31,13 @@ def getClues(guess, secretNum):
 
 def isOnlyDigits(num):
     # Returns True if num is a string of only digits. Otherwise, returns False.
-    
+    if num == '':
+        return False
+
+    for i in num:
+        if i not in '0 1 2 3 4 5 6 7 8 9'.split():
+            return False
+
+    return True
+
+
