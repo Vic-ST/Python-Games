225b574188594d3e5bf64ef0dd19570dc5222669
StevenCopeland <steven@thomasleecopeland.com>
2017-05-26 12:40:16 -0400

    fixing bugs
    fixing some of the bugs in the program.

--- animation.py
+++ animation.py
@@ -7,7 +7,7 @@ pygame.init()
 # Set up the window
 WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
-WINDOWSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation')
 
 # Set up direction variable
@@ -18,7 +18,7 @@ UPRIGHT = 'upright'
 
 MOVESPEED = 4
 
-#Set up the colors.
+# Set up the colors.
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
