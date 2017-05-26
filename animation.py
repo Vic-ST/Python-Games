02229f56ace28b538b697c1982a055aa093a2b8a
StevenCopeland <steven@thomasleecopeland.com>
2017-05-26 12:16:00 -0400

    Setting up colors

--- animation.py
+++ animation.py
@@ -11,3 +11,16 @@ WINDOWSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation')
 
 # Set up direction variable
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
+
+#Set up the colors.
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
