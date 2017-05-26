c5a552e3033930b14ca4e99e581156a67da0189f
StevenCopeland <steven@thomasleecopeland.com>
2017-05-26 12:28:31 -0400

    The game loop
    Move the box data structure.

--- animation.py
+++ animation.py
@@ -29,3 +29,31 @@ b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
 b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
 b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
 boxes = [b1, b2, b3]
+
+# Run the game loop.
+while True:
+    # Check for Quit event
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+            # Draw the white background on the surface.
+            windowSurface.fill(FILL)
+
+            for b in boxes:
+                # Move the box data structure.
+                if b['dir'] == DOWNLEFT:
+                    b['rect'].left -= MOVESPEED
+                    b['rect'].top += MOVESPEED
+                if b['dir'] == DOWNRIGHT:
+                    b['rect'].left += MOVESPEED
+                    b['rect'].top += MOVESPEED
+                if b['dir'] == UPLEFT:
+                    b['rect'].left -= MOVESPEED
+                    b['rect'].top -= MOVESPEED
+                if b['dir'] == UPRIGHT:
+                    b['rect'].left += MOVESPEED
+                    b['rect'].top -= MOVESPEED
+
+                # Check whether the box has moved out of the window.
