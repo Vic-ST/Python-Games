9d339ebbabcf95bf10ff21d2a6a288ce3ca8624f
StevenCopeland <steven@thomasleecopeland.com>
2017-05-27 17:49:47 -0400

    set up block
    adding an image and setting up the block data structure.

--- spritesAndSounds.py
+++ spritesAndSounds.py
@@ -4,7 +4,7 @@ from pygame.local import *
 # Set up pygame.
 pygame.init()
 
-# Set up the window
+# Set up the window.
 WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
@@ -14,3 +14,15 @@ pygame.display.set_caption('Sprites and Sounds')
 WHITE = (255, 255, 255)
 
 # Set up the block data structure.
+player.pygame.Rect(300, 100, 40, 40)
+playerImage = pygame.image.load('player.png')
+playerStretchedImag = pygame.transform.scale(playerImage,(40, 40))
+foodImage = pygame.image.load('cherry.png')
+foods = []
+for i in range(1,20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - 20), random.randint(0, WINDOWHEIGHT - 20), 20, 20))
+
+foodCounter = 0
+NEWFOOD = 40
+
+# Set up keyboard variables.
