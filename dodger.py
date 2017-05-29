f74bebf463c2e9049e1a05ea309201b524c35525
StevenCopeland <steven@thomasleecopeland.com>
2017-05-29 12:32:10 -0400

    Adding a new function
    Adding and compeleting a new function called waitForPlayerToPressKey

--- dodger.py
+++ dodger.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
 TEXTCOLOR = (0, 0, 0)
+BACKGROUNDCOLOR = (255, 255, 255)
+FPS = 60
 BADDIEMINSIZE = 10
 BADDIEMAXSIZE = 60
 BADDIEMINSPEED = 1
@@ -14,4 +16,14 @@ PLAYERMOVERATE = 5
 def terminate():
     pygame.quit()
     sys.exit()
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == ESCAPE: # Pressing ESC quits.
+                    terminate()
+                return
 
