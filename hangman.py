6c6e2c796be88c2ebbad22a9baac56034347b650
StevenCopeland <steven@thomasleecopeland.com>
2017-06-06 18:12:08 -0400

    completing a function
    completing the random word function.

--- hangman.py
+++ hangman.py
@@ -35,3 +35,10 @@ HANGMAN_PICS = ['''
    /|\  |
    / \  |
        ===''']
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra orange banana computer programming javascript ruby python video people night walking everything'.split()
+
+def getRandomWord(wordList):
+    # This function returns a random string from the passed list of strings.
+    wordIndex = randomint(0, len(wordList) - 1)
+    return wordList[wordIndex]
+
