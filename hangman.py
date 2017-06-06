063fb258ea8628f2d26de7e8477cbeabe7b035a5
StevenCopeland <steven@thomasleecopeland.com>
2017-06-06 18:29:53 -0400

    finishing another function
    finishing a function which gets the players guess.

--- hangman.py
+++ hangman.py
@@ -61,3 +61,18 @@ def displayBoard(missedLetters, correctLetters, secretWord):
         print(letter, end=' ')
     print()
 
+def getGuess(alreadyGuessed):
+    # Return the letter the player entered. This function makes sure the player entered a single letter and not something else.\
+    while True:
+        print('Guess a letter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER.')
+        else:
+            return guess
+
